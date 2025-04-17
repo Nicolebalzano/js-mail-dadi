909f74fbb5e106afa5a58e5f0a350e83f4bd2b92
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-17 18:01:16 +0200

    feat:struttura codice impostata + esercizio mail init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email.
+// NON USARE INCLUDES!
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+//RACCOLTA DATI mail
+// mail della lista
+// mail richiesta all'utente
+//messaggio stampato finale
+
+
+//ELABORAZIONE LOGICA mail
+//Crea un array con la lista delle mail che possono accedere
+// Prompt che chiede all'utente la sua mail
+// Controlla che la mail inserita dall'utente sia nella lista
+//Stampa messaggio del risultato della ricerca
+ const mailLista = ["Nicolebalzano@gmail.com", "Lauraparesi01@gmail.com", "Lorenzomassi@gmail.com", "carloaresi10@gmail.com", "swamy12@gmail.com", "kauastella@gmail.com", "anabelfiore@gmail.com", "manfredi99@gmail.com", "maria22@gmail.com", "juju33@gmail.com"];
+ const = prompt("Inserisci qui la tua mail");
+
+//OUTPUT mail
