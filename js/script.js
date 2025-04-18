744877ec87127f2e9f0b56784af50592d2308009
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-18 10:51:30 +0200

    feat: gioco dadi completato

--- js/script.js
+++ js/script.js
@@ -23,17 +23,47 @@
 // Prompt che chiede all'utente la sua mail
 // Controlla che la mail inserita dall'utente sia nella lista
 //Stampa messaggio del risultato della ricerca
-const mailList = ["Nicolebalzano@gmail.com", "Lauraparesi01@gmail.com", "Lorenzomassi@gmail.com", "carloaresi10@gmail.com", "swamy12@gmail.com", "kauastella@gmail.com", "anabelfiore@gmail.com", "manfredi99@gmail.com", "maria22@gmail.com", "juju33@gmail.com"];
-const inserisciMail = prompt("Inserisci qui la tua mail");
-for(let i = 0; i < mailList.length; i++) {
-    const curMail = mailList[i];
-    if (curMail === inserisciMail) {
-        console.log("mail trovata") 
-        break;
-    } else {
-        console.log("Mail non trovata")
-    }
-    }
+
+// OUTPUT mail
+// const mailList = ["Nicolebalzano@gmail.com", "Lauraparesi01@gmail.com", "Lorenzomassi@gmail.com", "carloaresi10@gmail.com", "swamy12@gmail.com", "kauastella@gmail.com", "anabelfiore@gmail.com", "manfredi99@gmail.com", "maria22@gmail.com", "juju33@gmail.com"];
+// const inserisciMail = prompt("Inserisci qui la tua mail");
 
 
-//OUTPUT mail
+
+// for(let i = 0; i < mailList.length; i++) {
+//     const curMail = mailList[i];
+//     console.log(curMail);
+//     if (curMail === inserisciMail) {
+//      let messageFound = "Mail trovata";
+//      console.log(messageFound);
+
+//         break;
+//     } else {
+//         let messageNotFound = "Mail non trovata";
+//         console.log(messageNotFound);
+
+//     }
+//     }
+
+
+// RACCOLTA DATI dado
+// numero random computer
+// numero random giocatore
+
+// ELABORAZIONE LOGICA dado
+// 1) genera un numero random per il giocatore
+// 2) genera un numero random per il computer
+// 3) confronta i numeri generati in base al punteggio più alto determina il vincitore
+
+// OUTPUT dado
+let randomNumberGamer = Math.floor(Math.random() *10) +1;
+console.log("Numero giocatore 1:" + " " + randomNumberGamer);
+let randomNumberComputer = Math.floor(Math.random() * 10) +1;
+console.log("Numero computer:" + " " + randomNumberComputer);
+if (randomNumberGamer > randomNumberComputer) {
+    console.log("Risultato: Giocatore 1 vince");
+} else if (randomNumberGamer === randomNumberComputer) {
+    console.log("Risultato: Pareggio");
+} else {
+    console.log("Risultato: Computer vince");
+}
